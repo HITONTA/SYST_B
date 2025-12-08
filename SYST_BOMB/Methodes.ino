67d32870c20cf7dd1aee0912048ab1c7b6ff8e49
HITONTA <nils.oulhen@etu.univ-tours.fr>
2025-12-08 18:01:25 +0100

    v1.11

--- SYST_BOMB/Methodes.ino
+++ SYST_BOMB/Methodes.ino
@@ -1,3 +1,8 @@
+MachineEtat::MachineEtat() {
+    currentState = State::IDLE;
+    lastStateChange = 0;
+}
+
 void detection() {
   a=sr04.Distance();
 }
@@ -31,7 +36,42 @@ bool ButtonBPConf() {
   return false;   // bouton non pressé
 }
 
-void MachineEtat::init
+void updateFSM() {
+  // Lecture temps RTC
+  dt = clock.getDateTime();
+  unsigned long currentTime = dt.hour * 3600UL + dt.minute * 60UL + dt.second;
 
+  switch (currentState) {
 
+    case VEILLE:
+      detection();
+      if (a < 15) { // ATTENTION SENS < INVERSER POUR TEST
+        currentState = PREPA_GEN;
+        entryTimePREPA_GEN = currentTime; // mémoriser le temps d'entrée
+        Serial.println("Entrée PREPA_GEN !");
+      }
+      break; // très important pour éviter le fall-through
 
+    case PREPA_GEN:
+      detection();
+
+      // Vérification du temps
+      if ((currentTime - entryTimePREPA_GEN) >= maxSecondsPREPA_GEN) {
+        Serial.println("Temps dépassé PREPA_GEN -> retour VEILLE !");
+        currentState = VEILLE;
+        // ici tu peux désactiver servo, LED, buzzer
+        break;
+      }
+
+      // Vérification bouton CONF pour passer à l'état suivant
+      if (ButtonBPConf()) {
+        currentState = PREPA_ACT;
+        Serial.println("Bouton CONF pressé -> PREPA_ACT");
+        break;
+      }
+
+      break;
+
+    // Tu peux ajouter les autres états ici...
+  }
+}
