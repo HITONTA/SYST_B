93ed1c1312372c391329155ceaf5e537f04797ab
HITONTA <nils.oulhen@etu.univ-tours.fr>
2025-12-08 15:42:15 +0100

    def états

--- SYST_BOMB/FSM.ino
+++ SYST_BOMB/FSM.ino
@@ -1,1 +1,13 @@
 // Fonctionnement de la machine à état
+
+enum class State{
+  VEILLE
+  PREPA_GEN
+  PREPA_ACT
+  PREPA_MODE_AUTO
+  PREPA_MODE_RETARD
+  ARM_AUTO
+  ARM_RETARD
+  BOOM
+  DESARM
+};
