9cbfb1e9db90f26b6055964fe207bb21aa5220db
HITONTA <nils.oulhen@etu.univ-tours.fr>
2025-12-08 15:07:00 +0100

    include
    aa

--- SYST_BOMB/SYST_BOMB.ino
+++ SYST_BOMB/SYST_BOMB.ino
@@ -1,3 +1,21 @@
+//On importe les librairies nécessaires
+
+#include <dht_nonblocking.h>
+#include <DS3231.h>
+#include <IRremote.hpp>
+#include <Keypad.h>
+#include <LedControl.h>
+#include <LiquidCrystal.h>
+#include <MFRC522.h>
+#include <MPU6050.h>
+#include <QMI8658C.h>
+#include <I2Cdev.h>
+#include <SR04.h>
+#include <pitches.h>
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
